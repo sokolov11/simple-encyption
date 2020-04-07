64fd7de55ae28561036e29cac4a8fda611eac8c1
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-04-07 15:58:21 +0300

    upd

---
+++ src/test/local.unit.test.ts
@@ -0,0 +1,7 @@
+import { encrypt, decrypt, forget } from '../resolve-event-encryption-local'
+
+describe('event encryption local', () => {
+  test('test', async () => {
+    // TODO: later
+  })
+})
